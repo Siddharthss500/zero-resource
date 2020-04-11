3314192ac917df8fcd39a4d8310c0e06c827bd1f
Siddharthss500 <siddharthss500@gmail.com>
2020-04-11 11:20:50 -0400

    Added based files needed

--- main.py
+++ main.py
@@ -1,4 +1,86 @@
+import argparse
+import random
+import torch
+import os
+import itertools
 
+import Model
+from loader import load_sentences, update_tag_scheme, word_mapping, augment_with_pretrained
+
+def main(args):
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    # Setup torch device
+    device = torch.device('cuda' if args.device else 'cpu')
+
+    # Check validity of arguments
+    assert os.path.isfile(args.train)
+    assert os.path.isfile(args.valid)
+    assert os.path.isfile(args.test)
+    assert args.tag_scheme in ['iob', 'iobes']
+    assert args.char_dim > 0 or args.word_dim > 0
+
+    # Initialize model
+    # model = Model(parameters=args)
+
+    # Data parameters
+    lower = args.lower
+    zeros = args.zeros
+    tag_scheme = args.tag_scheme
+
+    # Load sentences
+    train_sentences = load_sentences(args.train, lower, zeros)
+    valid_sentences = load_sentences(args.valid, lower, zeros)
+    test_sentences = load_sentences(args.test, lower, zeros)
+
+    # Use selected tagging scheme
+    update_tag_scheme(train_sentences, tag_scheme)
+    update_tag_scheme(valid_sentences, tag_scheme)
+    update_tag_scheme(test_sentences, tag_scheme)
+
+    # Create dictionary/mapping of words
+    dico_words_train = word_mapping(train_sentences, lower)[0]
+    words = list(itertools.chain.from_iterable([[w[0] for w in s] for s in valid_sentences + test_sentences]))
+    dico_words, word_to_id, id_to_word = augment_with_pretrained(dico_words_train.copy(), words)
+
+    # Index data
+
+    # Save mappings
+
+    # Build model
+
+    # Train network
 
 if __name__ == '__main__':
-    print("Let's start")
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--train', type=str, default='data/eng.train',
+                        help='train file path')
+    parser.add_argument('--valid', type=str, default='data/eng.testa',
+                        help='validation file path')
+    parser.add_argument('--test', type=str, default='data/eng.testb',
+                        help='test file path')
+    parser.add_argument('--tag_scheme', type=str, default="iobes",
+                        help='Tagging scheme (IOB or IOBES)')
+    parser.add_argument('--lower', type=int, default=0,
+                        help='Lowercase words')
+    parser.add_argument('--zeros', type=int, default=0,
+                        help='Replace digits with zeros')
+    parser.add_argument('--char_dim', type=int, default=25,
+                        help='Dimension of character embeddings')
+    parser.add_argument('--char_lstm_dim', type=int, default=25,
+                        help='Dimension of LSTM character embeddings')
+    parser.add_argument('--word_dim', type=int, default=100,
+                        help='Dimension of word embeddings')
+    parser.add_argument('--word_lstm_dim', type=int, default=100,
+                        help='Dimension of LSTM word embeddings')
+    parser.add_argument('--cap_dim', type=int, default=0,
+                        help='Capitalization feature dimension')
+    parser.add_argument('--seed', type=int, default=42,
+                        help='seed for model')
+    parser.add_argument('--device', type=int, default=1,
+                        help='Run model on cpu(0) or gpu(1 default)')
+
+    args = parser.parse_args()
+    main(args)
---
+++ utils.py
@@ -0,0 +1,105 @@
+import io
+
+def iob2(tags):
+    """
+    Check that tags have a valid IOB format.
+    Tags in IOB1 format are converted to IOB2.
+    """
+    for i, tag in enumerate(tags):
+        if tag == 'O':
+            continue
+        split = tag.split('-')
+        if len(split) != 2 or split[0] not in ['I', 'B']:
+            return False
+        if split[0] == 'B':
+            continue
+        elif i == 0 or tags[i - 1] == 'O':  # conversion IOB1 to IOB2
+            tags[i] = 'B' + tag[1:]
+        elif tags[i - 1][1:] == tag[1:]:
+            continue
+        else:  # conversion IOB1 to IOB2
+            tags[i] = 'B' + tag[1:]
+    return True
+
+
+def iob_iobes(tags):
+    """
+    IOB -> IOBES
+    """
+    new_tags = []
+    for i, tag in enumerate(tags):
+        if tag == 'O':
+            new_tags.append(tag)
+        elif tag.split('-')[0] == 'B':
+            if i + 1 != len(tags) and \
+               tags[i + 1].split('-')[0] == 'I':
+                new_tags.append(tag)
+            else:
+                new_tags.append(tag.replace('B-', 'S-'))
+        elif tag.split('-')[0] == 'I':
+            if i + 1 < len(tags) and \
+                    tags[i + 1].split('-')[0] == 'I':
+                new_tags.append(tag)
+            else:
+                new_tags.append(tag.replace('I-', 'E-'))
+        else:
+            raise Exception('Invalid IOB format!')
+    return new_tags
+
+
+def iobes_iob(tags):
+    """
+    IOBES -> IOB
+    """
+    new_tags = []
+    for i, tag in enumerate(tags):
+        if tag.split('-')[0] == 'B':
+            new_tags.append(tag)
+        elif tag.split('-')[0] == 'I':
+            new_tags.append(tag)
+        elif tag.split('-')[0] == 'S':
+            new_tags.append(tag.replace('S-', 'B-'))
+        elif tag.split('-')[0] == 'E':
+            new_tags.append(tag.replace('E-', 'I-'))
+        elif tag.split('-')[0] == 'O':
+            new_tags.append(tag)
+        else:
+            raise Exception('Invalid format!')
+    return new_tags
+
+def create_dico(item_list):
+    """
+    Create a dictionary of items from a list of list of items.
+    """
+    assert type(item_list) is list
+    dico = {}
+    for items in item_list:
+        for item in items:
+            if item not in dico:
+                dico[item] = 1
+            else:
+                dico[item] += 1
+    return dico
+
+
+def create_mapping(dico):
+    """
+    Create a mapping (item to ID / ID to item) from a dictionary.
+    Items are ordered by decreasing frequency.
+    """
+    sorted_items = sorted(dico.items(), key=lambda x: (-x[1], x[0]))
+    id_to_item = {i: v[0] for i, v in enumerate(sorted_items)}
+    item_to_id = {v: k for k, v in id_to_item.items()}
+    return item_to_id, id_to_item
+
+def load_vectors(fname):
+    """
+    Load the pretrained word-embeddings from FastText
+    """
+    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    n, d = map(int, fin.readline().split())
+    data = {}
+    for line in fin:
+        tokens = line.rstrip().split(' ')
+        data[tokens[0]] = map(float, tokens[1:])
+    return data
